f1e0d8ed89ccee697a043871c732e1dade5e62c7
Ilya <bak@doza.ru>
2022-01-31 13:17:50 +0300

    updated by User1

--- NetGraphWinForms/Program.cs
+++ NetGraphWinForms/Program.cs
@@ -8,7 +8,7 @@ namespace NetGraphWinForms
     static class Program
     {
         /// <summary>
-        /// Главная точка входа для приложения.
+        /// Главная точка входа для приложения. Edited from master-branch
         /// </summary>
         [STAThread]
         static void Main()
